3e11582b8242a3c91f5c0555000258b96f449a82
julia <julia.albq@gmail.com>
2023-03-31 12:34:53 -0300

    test: add tests to check use of keyboard shortcut to send entry edition and to return to dashboard

--- cypress/e2e/editEntry.cy.js
+++ cypress/e2e/editEntry.cy.js
@@ -25,14 +25,18 @@ describe("When editing entry", () => {
       .should("have.text", format(parseISO(date, "yyyy-MM-dd"), "M/d/yyyy"));
   });
 
+  it("sends edited entry successfully by keyboard shortcut", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.FormEntries();
+
     const label = "Cat food";
     const amount = "50.90";
     const date = "2022-02-02";
-    cy.getByTestId("EntryFormLabelInput").type('{selectall}{backspace}').type(label)
-    cy.getByTestId("EntryFormAmountInput").type('{selectall}{backspace}').type(amount);
-    cy.getByTestId("EntryFormDateInput").type(date);
 
-    cy.getByTestId("SaveButton").click();
+    cy.EditFormEntries();
+
+    cy.get("body").trigger("keydown", { key: "Enter" });
 
     cy.getByTestId("DashboardEntry")
       .findByTestId("DashboardEntryLabel")
@@ -43,5 +47,23 @@ describe("When editing entry", () => {
     cy.getByTestId("DashboardEntry")
       .findByTestId("DashboardEntryDate")
       .should("have.text", format(parseISO(date, "yyyy-MM-dd"), "M/d/yyyy"));
+  });
+
+  it.only("allows user to return to dashboard without editing", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.FormEntries();
+
+    cy.getByTestId("EditEntryButton").click();
+
+    cy.getByTestId("EntryFormLabelInput")
+    .type("{selectall}{backspace}")
+
+    cy.get("body").trigger("keydown", { key: "Escape" });
+
+    cy.getByTestId("DashboardEntries").should("have.length", 1);
+    cy.getByTestId("DashboardEntry")
+    .findByTestId("DashboardEntryLabel")
+    .should("have.text", "Groceries");
   })
-})
+});
