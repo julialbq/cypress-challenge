c1857b21ec5cdfe2f496d84e216bcfdba7470f6a
julia <julia.albq@gmail.com>
2023-03-31 12:33:18 -0300

    test: add new command to test 'edit entry successfully'

--- cypress/e2e/editEntry.cy.js
+++ cypress/e2e/editEntry.cy.js
@@ -6,7 +6,24 @@ describe("When editing entry", () => {
 
     cy.FormEntries();
 
-    cy.getByTestId("EditEntryButton").click();
+    const label = "Cat food";
+    const amount = "50.90";
+    const date = "2022-02-02";
+
+    cy.EditFormEntries();
+
+    cy.getByTestId("SaveButton").click();
+
+    cy.getByTestId("DashboardEntry")
+      .findByTestId("DashboardEntryLabel")
+      .should("have.text", label);
+    cy.getByTestId("DashboardEntry")
+      .findByTestId("DashboardEntryAmount")
+      .should("contain.text", amount);
+    cy.getByTestId("DashboardEntry")
+      .findByTestId("DashboardEntryDate")
+      .should("have.text", format(parseISO(date, "yyyy-MM-dd"), "M/d/yyyy"));
+  });
 
     const label = "Cat food";
     const amount = "50.90";
